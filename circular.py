6f56a03fdec7644e40dd7d2c6b81874bbb7a0afb
Gab1nh0 <gabriel507xs@gmail.com>
2024-05-05 00:06:29 -0500

    primer dropeo

--- circular.py
+++ circular.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 import customtkinter
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
 from PIL import Image, ImageTk
 
 
@@ -35,87 +36,88 @@ class Circular(tk.Frame):
         titulo = customtkinter.CTkLabel(master=izqFrame, text='Algoritmo de Punto Medio', width=40, height=28, fg_color='transparent', font=("Roboto", 18))
         titulo.place(x=10, y=10)
 
-        label = customtkinter.CTkLabel(master=izqFrame, text='x₁ =', width=40, height=28, font=("Roboto", 18, "bold"), fg_color='transparent')
+        label = customtkinter.CTkLabel(master=izqFrame, text='Xc =', width=40, height=28, font=("Roboto", 18, "bold"), fg_color='transparent')
         label.place(x=10, y=70)
 
-        self.x1 = customtkinter.CTkEntry(master=izqFrame, width=140, height=28, fg_color='transparent',)
-        self.x1.place(x=60, y=70)
-        self.x1.focus_set()
+        self.xc = customtkinter.CTkEntry(master=izqFrame, width=140, height=28, fg_color='transparent',)
+        self.xc.place(x=60, y=70)
+        self.xc.focus_set()
 
-
-        label = customtkinter.CTkLabel(master=izqFrame, text='y₁ =', width=40, height=28, font=("Roboto", 18,"bold"), fg_color='transparent')
+        label = customtkinter.CTkLabel(master=izqFrame, text='Yc =', width=40, height=28, font=("Roboto", 18,"bold"), fg_color='transparent')
         label.place(x=260, y=70)
 
-        self.y1 = customtkinter.CTkEntry(master=izqFrame, width=140, height=28, fg_color='transparent')
-        self.y1.place(x=300, y=70)
+        self.yc = customtkinter.CTkEntry(master=izqFrame, width=140, height=28, fg_color='transparent')
+        self.yc.place(x=300, y=70)
         
-
-        label = customtkinter.CTkLabel(master=izqFrame, text='x₂ =', width=40, height=28, font=("Roboto", 18,"bold"), fg_color='transparent')
+        label = customtkinter.CTkLabel(master=izqFrame, text='r =', width=40, height=28, font=("Roboto", 18,"bold"), fg_color='transparent')
         label.place(x=10, y=120)
 
-        self.x2 = customtkinter.CTkEntry(master=izqFrame, width=140, height=28, fg_color='transparent')
-        self.x2.place(x=60, y=120)
+        self.r = customtkinter.CTkEntry(master=izqFrame, width=140, height=28, fg_color='transparent')
+        self.r.place(x=60, y=120)
 
-        label = customtkinter.CTkLabel(master=izqFrame, text='y₂ =', width=40, height=28, font=("Roboto", 18, "bold"), fg_color='transparent')
-        label.place(x=260, y=120)
-
-        self.y2 = customtkinter.CTkEntry(master=izqFrame, width=140, height=28, fg_color='transparent')
-        self.y2.place(x=300, y=120)
-
-        button = customtkinter.CTkButton(master=izqFrame, text='Graficar', width=140, height=28, font=("Roboto", 18), command=self.button_eventBresen)
-        button.place(x=300, y=170)
+        button = customtkinter.CTkButton(master=izqFrame, text='Graficar', width=140, height=28, font=("Roboto", 18), command=self.button_eventPunto)
+        button.place(x=300, y=120)
     
-    def trazarBresenham(self, numx1, numy1, numx2, numy2):
-        dx = abs(numx2 - numx1)
-        dy = abs(numy2 - numy1)
-    
-        if numx1 < numx2:
-            incX = 1
-        else:
-            incX = -1
+    def trazarPuntomedio(self, xc, yc, r):
+        
+        pinicial = int(5/4) - r
+        p = [pinicial]
+        x = [0]
+        y = [r]
 
-        if numy1 < numy2:
-            incY = 1
-        else:
-            incY = -1
+        puntosx=[]
+        puntosy=[]
 
-        x = numx1
-        y = numy1
-        puntosbb = [(x, y)]
-
-        if dx > dy:
-            p = 2 * dy - dx
-        for i in range(0, dx):
-            if p >= 0:
-                y += incY
-                p += 2 * (dy - dx)
+        while x[-1] < y[-1]:
+            if p[-1] < 0:
+                x.append(x[-1] + 1)
+                y.append(y[-1])
+                p.append(p[-1] + (2 * x[-1]) + 1)
             else:
-                p += 2 * dy
-            x += incX
-            puntosbb.append((x, y))
-        else:
-            p = 2 * dx - dy
-        for i in range(0, dy):
-            if p >= 0:
-                x += incX
-                p += 2 * (dx - dy)
-            else:
-                p += 2 * dx
-            y += incY
-            puntosbb.append((x, y))
+                x.append(x[-1] + 1)
+                y.append(y[-1] - 1)
+                p.append(p[-1] + (2 * x[-1]) + 1 - (2 * y[-1]))
 
-        return puntosbb, dx, dy
+            puntosx.append(xc+x[-1])
+            puntosy.append(yc+y[-1])
+            puntosx.append(xc+y[-1])
+            puntosy.append(yc+x[-1])
+
+            puntosx.append(xc+x[-1])
+            puntosy.append(yc-y[-1])
+            puntosx.append(xc+y[-1])
+            puntosy.append(yc-x[-1])
+
+            puntosx.append(xc-x[-1])
+            puntosy.append(yc+y[-1])
+            puntosx.append(xc-y[-1])
+            puntosy.append(yc+x[-1])
+
+            puntosx.append(xc-x[-1])
+            puntosy.append(yc-y[-1])
+            puntosx.append(xc-y[-1])
+            puntosy.append(yc-x[-1])
+
+            puntosx.append(xc-r)
+            puntosy.append(yc)
+            puntosx.append(xc+r)
+            puntosy.append(yc)
+            puntosx.append(xc)
+            puntosy.append(yc+r)
+            puntosx.append(xc)
+            puntosy.append(yc-r)
+
+        return puntosx, puntosy, x[-1], y[-1]
 
 
     
-    def button_eventBresen(self): #funcion q obtiene los datos 
+    def button_eventPunto(self): 
         from screens import Home
-        numx1 = int(self.x1.get())
-        numy1 = int(self.y1.get())
-        numx2 = int(self.x2.get())
-        numy2 = int(self.y2.get())
+        xc = int(self.xc.get())
+        yc = int(self.yc.get())
+        r = int(self.r.get())
 
-        puntosbb, dx, dy = self.trazarBresenham(numx1, numy1, numx2, numy2)
+        puntosx, puntosy, dx, dy = self.trazarPuntomedio(xc, yc, r)
 
         # Crear la figura y el gráfico
         fig = Figure(figsize=(5, 4), dpi=100, facecolor='#222831')
@@ -124,23 +126,11 @@ class Circular(tk.Frame):
         plot.set_facecolor('#222831')
 
 
-        #frame izquierdo inferior con datos
-        izqInf = customtkinter.CTkFrame(master=self, width=560, height=40, fg_color='transparent')
-        izqInf.place(x=10, y=290)
-
-        dxLabel = customtkinter.CTkLabel(master=izqInf, text='dx = '+ str(dx), width=40, height=28, font=("Roboto", 18, "bold"), fg_color='transparent')
-        dxLabel.place(x=10, y=10)
-
-        dyLabel = customtkinter.CTkLabel(master=izqInf, text='dy = '+ str(dy), width=40, height=28, font=("Roboto", 18, "bold"), fg_color='transparent')
-        dyLabel.place(x=100, y=10)
-
-
         if self.treeview:
             self.treeview.destroy()
 
         tablaFrame = customtkinter.CTkFrame(master=self, width=560, height=400, fg_color="#222831")
         tablaFrame.place(x=30, y=360)
-        
 
         style = ttk.Style()
         style.configure("Treeview", background="#222831", foreground="white", font=("Roboto", 18), rowheight=30, bordercolor="#FFD369")
@@ -151,17 +141,17 @@ class Circular(tk.Frame):
         self.treeview.heading('Y', text='Y')
         self.treeview.pack()
 
-
         # Agregar los puntos a la tabla
-        for punto in puntosbb:
-            self.treeview.insert('', 'end', values=punto)
+        for i in range(len(puntosx)):
+            self.treeview.insert('', 'end', values=(puntosx[i], puntosy[i]))
 
         # Dibujar la línea
-        plot.plot(*zip(*puntosbb), marker='o', color='#FFD369')
-        plot.set_title('Algoritmo de Bresenham para trazar una línea', color='white')
-        plot.set_xlabel('X', color='white')
-        plot.set_ylabel('Y', color='white')
-        plot.grid(True, color='#FFFFFF')
+        plot.plot(puntosx, puntosy, 'bo')
+        plot.set_aspect('equal', adjustable='box')
+        plot.set_xlabel('X')
+        plot.set_ylabel('Y')
+        plot.set_title('Punto medio')
+        plot.grid(True)
 
         plot.spines['bottom'].set_color('white')
         plot.spines['left'].set_color('white')
@@ -182,5 +172,3 @@ class Circular(tk.Frame):
     def moverHome(self): #nos lleva al inicio
         from screens import Home
         self.controller.show_frame(Home)
-
-    
